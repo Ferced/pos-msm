b7ca3fabffbac6ad54c3cb0c7264e46bd1ff88ae
Joaquin Fernandez Cedeira <fcedeirajoaquin@gmail.com>
2019-06-24 23:09:17 -0300

    meti un absoluth path para el config

--- polls/funciones.py
+++ polls/funciones.py
@@ -744,7 +744,8 @@ nombreglobal2=""
 nombreglobal3=""
 configPOS = configparser.ConfigParser()
 configPOS.sections()
-configPOS.read('configPOS.ini')
+print os.path.dirname(os.path.abspath(__file__))+r'\configPOS.ini'
+configPOS.read(os.path.dirname(os.path.abspath(__file__))+r'\configPOS.ini')
 usuarioL=configPOS['Superintendencia']['Usuario']
 contraL=configPOS['Superintendencia']['Contra']
 browser=Browser()
